d2493129b42f4b91effa5c1d5fe13a2356fb7711
Surya Gullapalli <surya.gullapalli@ddu-india.com>
2016-04-03 10:30:28 +0530

    removed file

--- chess-line-mode.el
+++
@@ -1,8 +0,0 @@
-
-(add-hook 'books/chess-line-mode-hook 'books/narrow-mode)
-(setq auto-mode-alist 
-      (append '(
-                ("\\.clm\\'" . books/chess-mainline-mode))
-              auto-mode-alist))
-
-(provide 'books/chess-line)
